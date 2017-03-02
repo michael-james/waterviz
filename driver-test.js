8448b1b525990b2a06aa76a5ec1f4ec4b44f7978
Michael James <thelivingpc@gmail.com>
2017-03-01 23:58:04 -0500

    cleaned up code

--- driver-test.js
+++ driver-test.js
@@ -7,80 +7,37 @@ const board = new five.Board({
   io: new Raspi()
 });
 
-var count = 12;
-var yMin = 20;
-var yMax = 140;
-var tDur = 1000;
-var tDly = 500;
+var count = 12; // number of servos
+var yMin = 20; // global minimum degrees
+var yMax = 140; // global maximum degrees
+var tDur = 1000; // duration of movement in ms
+var tDly = 500; // duration of delay in ms
 
 board.on("ready", () => {
   console.log("Connected");
 
   var s = [];
+
+  // create servo instances
   for (var i = 0; i < count; i++) {
-    s.push(new five.Servo({controller: "PCA9685", pin: i, startAt: 0,}));
+    s.push(new five.Servo({
+      controller: "PCA9685",
+      pin: i,
+      startAt: 0,}));
   }
 
-  for (var i = s.length - 1; i >= 0; i--) {
-    // s[i].min();
-  }
-
+  // loop ever tDly ms
   setInterval(function() {
     process.stdout.write("new!");
+
+    // iterate through servos
     for (var i = s.length - 1; i >= 0; i--) {
       
+      // generate random degree then go to it over time tDur
       var degrees = yMin + Math.floor(Math.random() * (yMax - yMin));
       process.stdout.write(" " + degrees);
       s[i].to(degrees, tDur);
     }
     console.log();
   }, (tDur + tDly));
-
-    // Initialize the servo instance
-  // var a = new five.Servo({controller: "PCA9685", pin: 0,});
-  // var b = new five.Servo({controller: "PCA9685", pin: 1,});
-  // var a = new five.Servo({controller: "PCA9685", pin: 2,});
-  // var b = new five.Servo({controller: "PCA9685", pin: 3,});
-  // var a = new five.Servo({controller: "PCA9685", pin: 4,});
-  // var b = new five.Servo({controller: "PCA9685", pin: 5,});
-  // var a = new five.Servo({controller: "PCA9685", pin: 6,});
-  // var b = new five.Servo({controller: "PCA9685", pin: 7,});
-  // var a = new five.Servo({controller: "PCA9685", pin: 8,});
-  // var b = new five.Servo({controller: "PCA9685", pin: 9,});
-  // var a = new five.Servo({controller: "PCA9685", pin: 10,});
-  // var b = new five.Servo({controller: "PCA9685", pin: 11,});
-
-
-  // var degrees = 0;
-
-  // degrees = Math.floor(Math.random() * 180);
-  //   a.to(degrees, 10000);
-  //   degrees = Math.floor(Math.random() * 180);
-  //   b.to(degrees, 10000);
-
-  // a.min();
-  // b.min();
-  // // b.to(0);
-
-  // a.sweep({
-  //   range: [20, 140], 
-  //   interval: 10000,
-  //   // step: 10
-  // })
-
-  // b.sweep({
-  //   range: [20, 140], 
-  //   interval: 10000,
-  //   // step: 10
-  // })
-
-  // a.to(degrees).delay(1000).to(0);
-  // b.to(degrees).delay(1000).to(0);
-
-  // setInterval(function() {
-  //   degrees = Math.floor(Math.random() * 180);
-  //   a.to(degrees, 10000);
-  //   degrees = Math.floor(Math.random() * 180);
-  //   b.to(degrees, 10000);
-  // }, 12000);
 });
