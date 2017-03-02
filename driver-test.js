0254e0df83eb75a9f7bb8fdb1e412d3a7b26abfa
Michael James <thelivingpc@gmail.com>
2017-03-01 23:48:51 -0500

    motor array working (random

--- driver-test.js
+++ driver-test.js
@@ -7,32 +7,80 @@ const board = new five.Board({
   io: new Raspi()
 });
 
+var count = 12;
+var yMin = 20;
+var yMax = 140;
+var tDur = 1000;
+var tDly = 500;
+
 board.on("ready", () => {
   console.log("Connected");
 
-  // Initialize the servo instance
-  var a = new five.Servo({
-    address: 0x40,
-    controller: "PCA9685",
-    range: [0, 180],
-    pin: 2,
-  });
+  var s = [];
+  for (var i = 0; i < count; i++) {
+    s.push(new five.Servo({controller: "PCA9685", pin: i, startAt: 0,}));
+  }
 
-  var b = new five.Servo({
-    address: 0x40,
-    controller: "PCA9685",
-    range: [0, 180],
-    pin: 3,
-  });
+  for (var i = s.length - 1; i >= 0; i--) {
+    // s[i].min();
+  }
 
-  var degrees = 90;
+  setInterval(function() {
+    process.stdout.write("new!");
+    for (var i = s.length - 1; i >= 0; i--) {
+      
+      var degrees = yMin + Math.floor(Math.random() * (yMax - yMin));
+      process.stdout.write(" " + degrees);
+      s[i].to(degrees, tDur);
+    }
+    console.log();
+  }, (tDur + tDly));
+
+    // Initialize the servo instance
+  // var a = new five.Servo({controller: "PCA9685", pin: 0,});
+  // var b = new five.Servo({controller: "PCA9685", pin: 1,});
+  // var a = new five.Servo({controller: "PCA9685", pin: 2,});
+  // var b = new five.Servo({controller: "PCA9685", pin: 3,});
+  // var a = new five.Servo({controller: "PCA9685", pin: 4,});
+  // var b = new five.Servo({controller: "PCA9685", pin: 5,});
+  // var a = new five.Servo({controller: "PCA9685", pin: 6,});
+  // var b = new five.Servo({controller: "PCA9685", pin: 7,});
+  // var a = new five.Servo({controller: "PCA9685", pin: 8,});
+  // var b = new five.Servo({controller: "PCA9685", pin: 9,});
+  // var a = new five.Servo({controller: "PCA9685", pin: 10,});
+  // var b = new five.Servo({controller: "PCA9685", pin: 11,});
+
+
+  // var degrees = 0;
+
+  // degrees = Math.floor(Math.random() * 180);
+  //   a.to(degrees, 10000);
+  //   degrees = Math.floor(Math.random() * 180);
+  //   b.to(degrees, 10000);
+
+  // a.min();
+  // b.min();
+  // // b.to(0);
+
+  // a.sweep({
+  //   range: [20, 140], 
+  //   interval: 10000,
+  //   // step: 10
+  // })
+
+  // b.sweep({
+  //   range: [20, 140], 
+  //   interval: 10000,
+  //   // step: 10
+  // })
 
   // a.to(degrees).delay(1000).to(0);
   // b.to(degrees).delay(1000).to(0);
 
-  setInterval(function() {
-    degrees = Math.floor(Math.random() * 100);
-    a.to(degrees);
-    b.to(degrees);
-  }, 1000);
+  // setInterval(function() {
+  //   degrees = Math.floor(Math.random() * 180);
+  //   a.to(degrees, 10000);
+  //   degrees = Math.floor(Math.random() * 180);
+  //   b.to(degrees, 10000);
+  // }, 12000);
 });
