8d77a4fca7cc68d8292b5a74526c55371b3b9382
Michael James <thelivingpc@gmail.com>
2017-04-28 00:53:20 -0400

    added flexibility
    angle offsets and array-based random data

--- driver-test.js
+++ driver-test.js
@@ -11,6 +11,21 @@ var yMax = 140; // global maximum degrees
 var tDur = 1000; // duration of movement in ms
 var tDly = 500; // duration of delay in ms
 
+var offsets = [
+    [ 0, 0, 0],
+    [ 1, 0, 0],
+    [ 2, 0, 0],
+    [ 3, 0, 0],
+    [ 4, 0, 0],
+    [ 5, 0, 0],
+    [ 6, 0, 0],
+    [ 7, 0, 0],
+    [ 8, 0, 0],
+    [ 9, 0, 0],
+    [10, 0, 0],
+    [11, 0, 0]
+];
+
 board.on("ready", () => {
   console.log("Connected");
 
@@ -28,12 +43,20 @@ board.on("ready", () => {
   setInterval(function() {
     process.stdout.write("new!");
 
+    var data = [];
+    for (var i = 0; i < count; i++) {
+      data.push(Math.random());
+    }
+
     // iterate through servos
     for (var i = s.length - 1; i >= 0; i--) {
+
+      var myMin = yMin + offsets[i][1];
+      var myMax = yMax + offsets[i][2];
       
       // generate random degree then go to it over time tDur
-      var degrees = yMin + Math.floor(Math.random() * (yMax - yMin));
-      process.stdout.write(" " + degrees);
+      var degrees = myMin + Math.floor(data[i] * (myMax - myMin));
+      process.stdout.write(" " + (data[i] * 100) + "/" + degrees);
       s[i].to(degrees, tDur);
     }
     console.log();
