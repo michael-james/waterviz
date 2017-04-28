dbc1a62e90f5a90c1c2e2a6773c70d26dbff14a3
Michael James <thelivingpc@gmail.com>
2017-04-28 01:06:48 -0400

    commenting

--- driver-test.js
+++ driver-test.js
@@ -1,30 +1,26 @@
 
 const Raspi = require('raspi-io');
 const five = require('johnny-five');
-const board = new five.Board({
-  io: new Raspi()
-});
+const board = new five.Board({ io: new Raspi() });
 
-var count = 12; // number of servos
-var yMin = 20; // global minimum degrees
-var yMax = 140; // global maximum degrees
-var tDur = 1000; // duration of movement in ms
-var tDly = 500; // duration of delay in ms
+///////////////////////////////////////////////////
+// set variables
+///////////////////////////////////////////////////
+
+var count = 12;   // number of servos
+var yMin = 20;    // global minimum degrees
+var yMax = 140;   // global maximum degrees
+var tDur = 1000;  // duration of movement in ms
+var tDly = 500;   // duration of delay in ms
 
 var offsets = [
-    [ 0, 0, 0],
-    [ 1, 0, 0],
-    [ 2, 0, 0],
-    [ 3, 0, 0],
-    [ 4, 0, 0],
-    [ 5, 0, 0],
-    [ 6, 0, 0],
-    [ 7, 0, 0],
-    [ 8, 0, 0],
-    [ 9, 0, 0],
-    [10, 0, 0],
-    [11, 0, 0]
+    [ 0, 0, 0], [ 1, 0, 0], [ 2, 0, 0], [ 3, 0, 0], [ 4, 0, 0], [ 5, 0, 0],
+    [ 6, 0, 0], [ 7, 0, 0], [ 8, 0, 0], [ 9, 0, 0], [10, 0, 0], [11, 0, 0]
 ];
+
+///////////////////////////////////////////////////
+// control actuators
+///////////////////////////////////////////////////
 
 board.on("ready", () => {
   console.log("Connected");
@@ -39,10 +35,11 @@ board.on("ready", () => {
       startAt: 0,}));
   }
 
-  // loop ever tDly ms
+  // loop (period is duration of movement + duration of delay)
   setInterval(function() {
     process.stdout.write("new!");
 
+    // create array of random values (0.0 - 1.0)
     var data = [];
     for (var i = 0; i < s.length; i++) {
       data.push(Math.random());
@@ -50,14 +47,15 @@ board.on("ready", () => {
 
     // iterate through servos
     for (var i = 0; i < s.length; i++) {
+      // apply offsets to global range
       var myMin = yMin + offsets[i][1];
       var myMax = yMax + offsets[i][2];
       
-      // generate random degree then go to it over time tDur
+      // convert data to my degrees then move to
       var degrees = myMin + Math.floor(data[i] * (myMax - myMin));
-      process.stdout.write(" " + (data[i] * 100) + "/" + degrees);
       s[i].to(degrees, tDur);
+      process.stdout.write(" " + (data[i] * 100) + "/" + degrees);
     }
-    console.log();
+    console.log(); // print info to new line
   }, (tDur + tDly));
 });
