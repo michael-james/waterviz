7e94f9aad291437c0c21e977010951cd87f0215a
Michael James <thelivingpc@gmail.com>
2017-04-29 20:00:01 -0400

    added move alone test

--- drive.js
+++ drive.js
@@ -8,8 +8,8 @@ const board = new five.Board({ io: new Raspi() });
 ///////////////////////////////////////////////////
 
 var count = 12;   // number of servos
-var yMin = 20;    // global minimum degrees
-var yMax = 140;   // global maximum degrees
+var gMin = 20;    // global minimum degrees
+var gMax = 140;   // global maximum degrees
 var tDur = 1000;  // duration of movement in ms
 var tDly = 500;   // duration of delay in ms
 
@@ -21,6 +21,8 @@ var offsets = [
 ///////////////////////////////////////////////////
 // control actuators
 ///////////////////////////////////////////////////
+
+var tTot = tDur + tDly;   // duration of delay in ms
 
 board.on("ready", () => {
   console.log("Connected");
@@ -45,17 +47,44 @@ board.on("ready", () => {
       data.push(Math.random());
     }
 
-    // iterate through servos
+    moveTogether(data);
+    // moveAlone(data);
+    
+    console.log(); // print info to new line
+  }, (tTot));
+});
+
+function moveTogether(data) {
+  // iterate through servos
     for (var i = 0; i < s.length; i++) {
       // apply offsets to global range
-      var myMin = yMin + offsets[i][1];
-      var myMax = yMax + offsets[i][2];
+      var myMin = gMin + offsets[i][1];
+      var myMax = gMax + offsets[i][2];
       
       // convert data to my degrees then move to
       var degrees = myMin + Math.floor(data[i] * (myMax - myMin));
       s[i].to(degrees, tDur);
-      process.stdout.write(" " + (data[i] * 100) + "/" + degrees);
+      process.stdout.write(" " + (data[i] * 100) + ":" + degrees);
     }
-    console.log(); // print info to new line
-  }, (tDur + tDly));
-});
+}
+
+function moveAlone(data) {
+  (function delayWrapper (i) {
+    var dly = 0;
+    var myMin = gMin + offsets[i][1];
+    var myMax = gMax + offsets[i][2];
+    var myArc = myMax - myMin;
+    var myAngle = Math.floor(data[i] * (myMax - myMin));
+    var degrees = myMin + myAngle;
+    var myDur = myAngle / myArc * tDur;
+    
+    console.log("go! " + i + ":" + (data[i] * 100) + ":" + degrees);
+    s[i].to(degrees, myDur);
+
+    setTimeout(function () {
+      if (--i) {          // If i > 0, keep going
+        delayWrapper(i);       // Call the loop again, and pass it the current value of i
+      }
+    }, myDur);
+  })(s.length);
+}
