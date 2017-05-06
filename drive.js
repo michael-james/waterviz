32c5195a6c83433151127e8b44faa36115b54b80
Michael James <thelivingpc@gmail.com>
2017-05-06 17:03:43 -0400

    style changes for clarity

--- drive.js
+++ drive.js
@@ -4,34 +4,37 @@ const five = require('johnny-five');
 const board = new five.Board({ io: new Raspi() });
 
 ///////////////////////////////////////////////////
-// set variables
+// set global variables
 ///////////////////////////////////////////////////
 
 var count = 10;        // number of servos
 var gMin = 35;         // global minimum degrees
-// var gMax = 180;        // global maximum degrees
-// var gMin = 50;         // global minimum degrees
 var gMax = 135;        // global maximum degrees
 var tDur = 24000;      // duration of movement in ms
 var tDly = 5000;       // duration of delay in ms
 var constRate = false;  // movement always at same speed?
-var randOrder = false;  // set values out of order
-var fakeMax = true;
-var valMax = false;
-var stop = false;
+var randOrder = false;  // moves servos alone out of order
+var fakeMax = true;		// force water to go full range instead of random values
+var valMax = false;		// game variable (used to switch between max and min)
+var stop = false;		// used for emergency stop sequence
 
+// 0 = number, 1 = min offset, 2 = max offset
 var offsets = [
     [ 0, 0, 0], [ 1, 0, 0], [ 2, 0, 0], [ 3, 0, 0], [ 4, 0, 0], [ 5, 0, 0],
     [ 6, 0, 0], [ 7, 0, 0], [ 8, 0, 0], [ 9, 0, 0], [10, 0, 0], [11, 0, 0]
 ];
 
 ///////////////////////////////////////////////////
-// control actuators
+// default global variables
 ///////////////////////////////////////////////////
 
 var tTot = tDur + tDly;   // duration of delay in ms
 var s = []; // servos
 var zero = [0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0];
+
+///////////////////////////////////////////////////
+// control actuators
+///////////////////////////////////////////////////
 
 board.on("ready", () => {
   console.log("Connected");
@@ -44,22 +47,16 @@ board.on("ready", () => {
     s.push(new five.Servo({
       controller: "PCA9685",
       pin: i,
-      // range: [myMin, myMax],
-      startAt: 0,}));
+      startAt: 0,})); // sets the location all motors go to on startup
   }
 
-  // get current positions
-  // for (var i = 0; i < s.length; i++) {
-  //   console.log(i + " = " + s[i].position);
-  // }
-
-  // set to relative "zero"
+  // set to relative "zero" (gMin + offset)
   console.log("\n=== move all to myMin ===");
   for (var e = 0; e < s.length; e++) {
     var myMin = gMin + offsets[e][1];
     var myDur = tDur / 2;
 
-    s[e].to(myMin, myDur);
+    s[e].to(myMin, myDur); // moves servo to myMin degrees over myDur ms
     console.log("servo " + e + " to " + myMin + " degrees in " + myDur + "ms");
   }
 
@@ -77,13 +74,13 @@ function moveTogether(data, literal) {
     // apply offsets to global range
     var myMin = gMin + offsets[i][1];
     var myMax = gMax + offsets[i][2];
-    var myDur = tDur * 2;
+    var myDur = tDur * 2; // when things are moved together, they are twice as slow
     
     // convert data to my degrees then move to
     var degrees = myMin + Math.floor(data[i] * (myMax - myMin));
     if (literal) {degrees = data[i]};
 
-    s[i].to(degrees, myDur);
+    s[i].to(degrees, myDur); // moves servo to degrees over myDur ms
     console.log("servo " + i + " to " + degrees + " degrees in " + myDur + "ms");
   }
 }
@@ -93,13 +90,15 @@ function moveAlone(data) {
   if (randOrder) var myOrder = makeOrder();
 
   function delayWrapper(i) {
+  	// if movement has not been stopped, move next servo
     if (!stop) {
-      // console.log(i);
+    	// if randOrder true, uses random order
       var j = i;
       if (randOrder) {
         j = myOrder[i];
       }
-      var dly = 0;
+
+      // my variables
       var myMin = gMin + offsets[j][1];
       var myMax = gMax + offsets[j][2];
       var myArc = myMax - myMin;
@@ -109,32 +108,25 @@ function moveAlone(data) {
       if (constRate) {
         myDur = Math.floor(myAngle / myArc * tDur)
       };
+      // if servo set to 0, double duration of movement (makes it slower when moving down to zero)
       if (data[j] == 0) {myDur = myDur * 2;}
       
       console.log("servo " + j + " to " + degrees + " degrees in " + myDur + "ms");
-      s[j].to(degrees, myDur);
+      s[j].to(degrees, myDur); // moves servo to degrees over myDur ms
 
+      // determines whether to continue looping
       setTimeout(function () {
+        // if this is the last servo, stop looping
         if (i < (s.length - 1)) {          // If i > 0, keep going
           i = i + 1;
+          // omits servos 1 and 7
           if (i == 1 || i == 7) {
             i = i + 1;
           }
-          delayWrapper(i);       // Call the loop again, and pass it the current value of i
+          delayWrapper(i);       // Call the loop again, and passes it the next servo
         } 
         else {
-          // moveAll(0);
-
-          // var zero = [0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0];
-          // console.log(zero);
-          // moveAlone(zero);
-
-          // randOrder = !randOrder;
-
-          // setTimeout(function() {
-          //   moveAlone(newData());
-          // }, tTot);
-
+        	// start another full loop after tDly
           setTimeout(function() {
             moveAlone(newData());
           }, tDly);
@@ -143,9 +135,11 @@ function moveAlone(data) {
     }
   };
 
+  // starts the recursion
   delayWrapper(0);
 }
 
+// wrapper for moveTogether; moves all servos to val
 function moveAll(val) {
   var data = [];
   for (var i = 0; i < s.length; i++) {
@@ -156,22 +150,23 @@ function moveAll(val) {
   moveTogether(data, true);
 }
 
+// generates new array of random data (or fake values)
 function newData(zero) {
   var data = [];
+  // switches the valMax variable between 0 and 1 during fakeMax
   valMax = !valMax;
   var val = valMax * 1.0;
-  // if (zero) {
-  //   val = 0;
-  // }
+
+  // creates array
   for (var i = 0; i < s.length; i++) {
+  	// if fakeMax is off, random value, else use values from above
     if (!fakeMax) {val = Math.random();}
-    // if (i % 2 == 0) {val = Math.random();}
     data.push(val);
   }
   return data;
 }
 
-// create an array with randomly ordered consecutive indexes
+// create an array with randomly ordered consecutive indexes for randomizing servo order
 function makeOrder() {
 // first make a new array
   var order = [];
@@ -194,27 +189,6 @@ function makeOrder() {
   return order;
 }
 
-///////////////////////////////////////////////////
-// exit
-///////////////////////////////////////////////////
-
-// process.stdin.resume();//so the program will not close instantly
-
-function exitHandler(options, err) {
-    if (options.cleanup) moveAll(0);
-    if (err) console.log(err.stack);
-    if (options.exit) moveAll(0);
-}
-
-//do something when app is closing
-process.on('exit', exitHandler.bind(null,{cleanup:true}));
-
-// catches ctrl+c event
-process.on('SIGINT', exitHandler.bind(null, {exit:true}));
-
-// //catches uncaught exceptions
-// process.on('uncaughtException', exitHandler.bind(null, {exit:true}));
-
 /////////////////////////////////////////////////
 // keypress
 /////////////////////////////////////////////////
@@ -222,7 +196,7 @@ process.on('SIGINT', exitHandler.bind(null, {exit:true}));
 var keypress = require('keypress');
 
 var timePressed = 0;
-var delta = 1000;
+var delta = 1000; // amount of delay for debouncing
  
 // make `process.stdin` begin emitting "keypress" events 
 keypress(process.stdin);
@@ -234,23 +208,25 @@ process.stdin.on('keypress', function (ch, key) {
   if (key && key.ctrl && key.name == 'c') {
     process.stdin.pause();
   }
-  if (key && key.name == 'h') {
-    // moveAlone(zero);
 
+  // if 'h' key is pressed, start exit procedure
+  if (key && key.name == 'h') {
+
+  	// debouncer
     var now = new Date().getTime();
     console.log(timePressed, now, now - timePressed);
 
     if ((now - timePressed) > delta) {
       console.log("stopping...");
-      stop = true;
-      // stop servos
+      stop = true; // set stop variable to stop loops
+      // send stop command to servos
       for (var i = 0; i < s.length; i++) {
         s[i].stop();
       }
 
       console.log("resetting to home... waiting to exit...");
-      setTimeout(function() {moveAll(0);}, (tDly));
-      setTimeout(function() {process.stdin.pause();}, (tDur + tDly));
+      setTimeout(function() {moveAll(0);}, (tDly)); // move all to zero
+      setTimeout(function() {process.stdin.pause();}, (tDur + tDly)); // end process
     }
 
     timePressed = new Date().getTime();
@@ -259,19 +235,4 @@ process.stdin.on('keypress', function (ch, key) {
  
 process.stdin.setRawMode(true);
 process.stdin.resume();
-
-
-///////////////////////////////////////////////////
-// junk
-///////////////////////////////////////////////////
-
-/*
-  // loop (period is duration of movement + duration of delay)
-  setInterval(function() {
-    process.stdout.write("new!");
-    
-    // console.log(); // print info to new line
-  }, (1000 * 10));
-*/
-
 
